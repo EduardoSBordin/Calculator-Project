44e58d006ee4721fe61673aeda81faeb46c4c8f2
Eduardo Bordin <dudubordin26@gmail.com>
2022-04-13 01:30:05 -0300

    Testando botão

---
+++ testescript.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('.body');
+//const btn = document.querySelector('.btn');
+
+/* btn.onclick = */ function theme(){
+
+    //alert('Teste');
+
+//    this.classList.toggle('active');
+    body.classList.toggle('active');
+}
